780e29fd411e378229bc6635b7c89d809377b0a9
vijaytyagi369 <vijaytyagi1998@gmail.com>
2020-09-24 18:46:47 +0530

    bSort method is Complete.

--- src/sortingAlgo/bSort.java
+++ src/sortingAlgo/bSort.java
@@ -2,11 +2,32 @@ package sortingAlgo;
 
 public class bSort {
     public static void swap(int[] arr, int i,int j){
-        int temp=i;
+        int temp=arr[i];
         arr[i]=arr[j];
         arr[j]=temp;
 
     }
+     public static void bubbleSort(int[] array){
+        boolean isSorted=false;
+        int lastSorted=array.length-1;
+        while (!isSorted){
+            isSorted=true;
+            for (int i = 0; i <lastSorted ; i++) {
+                if(array[i]>array[i+1]){
+                    swap(array,i,i+1);
+                    isSorted=false;
+                }
+            }
+            lastSorted--;
+        }
+    }
 
-
+    public static void main(String[] args) {
+        System.out.println("Enter the Array To Be Sorted ");
+        int[] arr={5,4,3,2,1};
+        bubbleSort(arr);
+        for (int i=0;i<5;i++){
+            System.out.println(arr[i]);
+        }
+    }
 }
